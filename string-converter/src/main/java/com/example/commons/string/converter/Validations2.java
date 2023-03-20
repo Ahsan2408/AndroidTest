70a58f4d51994f8b94951682443b1d2df2ccd232
Ahsan Murtaza <ahsan.chahsan74@gmail.com>
2023-03-20 17:56:13 +0500

    Add validaiton2 class

---
+++ string-converter/src/main/java/com/example/commons/string/converter/Validations2.java
@@ -0,0 +1,10 @@
+package com.example.commons.string.converter;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class Validations2 {
+    public static void showToast(Context context, String message) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    }
+}
