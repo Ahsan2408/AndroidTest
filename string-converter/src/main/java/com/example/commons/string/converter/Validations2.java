32beab02326ea2308e3625c01281ac43744b38b8
Ahsan Murtaza <ahsan.chahsan74@gmail.com>
2023-03-22 17:21:56 +0500

    add ShowToast func in Validation2 class

--- string-converter/src/main/java/com/example/commons/string/converter/Validations2.java
+++ string-converter/src/main/java/com/example/commons/string/converter/Validations2.java
@@ -7,4 +7,7 @@ public class Validations2 {
     public static void showToast(Context context, String message) {
         Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
     }
+    public static void showToast22(Context context, String message) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    }
 }
