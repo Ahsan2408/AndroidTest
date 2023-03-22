53129e08feb8e7b31e30f98cafe7726aac75976c
Ahsan Murtaza <ahsan.chahsan74@gmail.com>
2023-03-22 16:40:10 +0500

    Add validation3 class

---
+++ string-converter/src/main/java/com/example/commons/string/converter/Validations3.java
@@ -0,0 +1,10 @@
+package com.example.commons.string.converter;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class Validations3 {
+    public static void showAhsanToast(Context context, String message) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    }
+}
